fce29a3e07b764acb994a0001b211e40b310eccc
Crowdbotics <team@crowdbotics.com>
2021-08-12 09:22:46 +0000

    Initial commit.

---
+++ options/options.js
@@ -0,0 +1,6 @@
+export const globalOptions = {
+  name: "wild_sky_29755Identifier",
+  url: "https://wild_sky_29755Identifier.botics.co",
+  api: "https://wild_sky_29755Identifier.botics.co/api/v1"
+}
+export const modulesOptions = {}
